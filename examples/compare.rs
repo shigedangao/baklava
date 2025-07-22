c0d8400d7b18a44f4b4c73c5345566c386c8633e
shigedangao <marc.inthaamnouay@gmail.com>
2025-07-22 23:57:48 +0200

    support multiple image sources to compare the target img

--- examples/compare.rs
+++ examples/compare.rs
@@ -1,9 +1,12 @@
-use baklava::InsightFace;
+use baklava::{InsightFace, Methodology};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (cosine, percentage) = InsightFace::new("./Megatron")?
-        .prepare_images(&["./face1_test.png", "./face2_test.png"])?
-        .compare_images()?;
+    let (cosine, percentage) = InsightFace::new("./Megatron", 3)?
+        .prepare_images(
+            &["./face1_test.png", "./face2_test.png"],
+            "./face2_test.png",
+        )?
+        .compare_images(Methodology::Median)?;
 
     println!("cosine: {cosine}");
     println!("percentage: {percentage}");
